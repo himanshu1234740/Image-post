8600212e344b2eaddedfe5fc91a3ef9cb7b2ab35
Himanshu <gautamhimanshu689@gmail.com>
2022-10-27 17:11:51 +0530

    set update

--- asets/js/index.js
+++ asets/js/index.js
@@ -1,11 +1,10 @@
-
 function getCookie(cname) {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
+  let ca = decodedCookie.split(";");
   for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
-    while (c.charAt(0) == ' ') {
+    while (c.charAt(0) == " ") {
       c = c.substring(1);
     }
     if (c.indexOf(name) == 0) {
@@ -14,13 +13,10 @@ function getCookie(cname) {
   }
   return "";
 }
-let get_cookie = getCookie('jwt')
+let get_cookie = getCookie("jwt");
 if (get_cookie) {
-  
-
   if (localStorage.getItem("user")) {
-
-    let user = JSON.parse(localStorage.getItem("user"))
+    let user = JSON.parse(localStorage.getItem("user"));
     let first_letter = user.user;
     let uppercase = first_letter[0].toUpperCase();
     let remainStr = "";
@@ -29,58 +25,48 @@ if (get_cookie) {
     }
     let sort = remainStr.slice(0, 11);
     document.getElementById("username").innerHTML = "My Account";
-  
-    document.getElementById("login_user").style.visibility = "hidden";
-  
-  }
 
-}else {
+    document.getElementById("login_user").style.visibility = "hidden";
+  }
+} else {
   document.getElementById("username").style.visibility = "hidden";
 }
 
-
-
-
-
-
-
-
 function post() {
-  let img = document.getElementById('email').files[0];
-  let desc = document.getElementById('desc').value;
-  let title = document.getElementById('title').value;
+  let img = document.getElementById("email").files[0];
+  let desc = document.getElementById("desc").value;
+  let title = document.getElementById("title").value;
 
   let formdata = new FormData();
 
   formdata.append("img", img, img.filename);
   formdata.append("desc", desc);
-  formdata.append("title", title)
+  formdata.append("title", title);
 
-
-  axios.post(`/api/users`, formdata).then(function (response) {
-
-
-    window.location.href = '/';
-
-  }).catch(function (error) {
-    console.error(error);
-  });
+  axios
+    .post(`/api/users`, formdata)
+    .then(function (response) {
+      window.location.href = "/";
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 }
 // here we are delete the post
 function del(id) {
-
   const options = {
-    method: 'DELETE',
+    method: "DELETE",
     url: `api/users/${id}`,
-
   };
 
-  axios.request(options).then(function (response) {
-    window.location.href = '/';
-
-  }).catch(function (error) {
-    console.log(error);
-  });
+  axios
+    .request(options)
+    .then(function (response) {
+      window.location.href = "/";
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
 function update(id) {
@@ -88,33 +74,48 @@ function update(id) {
   let desc = document.getElementById("update_desc").value;
   let title = document.getElementById("update_title").value;
   let formdata = new FormData();
-  formdata.append("img", img, img.filename);
-  formdata.append("desc", desc);
-  formdata.append("title", title);
-
-
-  axios.put(`api/users/${id}`, formdata).then(function (response) {
-    window.location.href = '/';
+  
+  if (img === undefined) {
+    console.log("undefined img");
+    axios
+      .put(`api/users/${id}`, { desc, title })
+      .then(function (response) {
+        window.location.href = '/';
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  } else {
     
+    formdata.append("img", img, img.filename);
+    formdata.append("desc", desc);
+    formdata.append("title", title);
 
-  }).catch(function (error) {
-    console.error(error);
-  });
+    axios
+      .put(`api/users/${id}`, formdata)
+      .then(function (response) {
+        window.location.href = '/';
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
 }
 
-
 // search functionality
-let btn = document.getElementById('input');
+let btn = document.getElementById("input");
 
 function search() {
-  let card = document.getElementsByClassName('col')
-  let input = document.getElementById('input').value.toLowerCase();
+  let card = document.getElementsByClassName("col");
+  let input = document.getElementById("input").value.toLowerCase();
   for (let i = 0; i < card.length; i++) {
-    let val = card[i].getElementsByTagName('h5')[0].innerHTML.toLowerCase();
+    let val = card[i].getElementsByTagName("h5")[0].innerHTML.toLowerCase();
     if (val.indexOf(input) > -1) {
       card[i].style.display = "";
     } else {
-      card[i].style.display = "none"
+      card[i].style.display = "none";
     }
   }
 }
@@ -122,8 +123,8 @@ function search() {
 async function login() {
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
-  console.log(password, email)
-  const { data } = await axios.post('/api/login', { email, password });
+  console.log(password, email);
+  const { data } = await axios.post("/api/login", { email, password });
   console.log(data.password);
   localStorage.setItem("user", JSON.stringify(data));
 
@@ -131,14 +132,9 @@ async function login() {
     swal("Oops!", "You Put Invalid Detail!", "error");
   } else if (data === "err") {
     swal("Oops!", "You Put Invalid Detail!", "error");
-  }
-
-  else {
-
+  } else {
     window.location.href = "/";
   }
-
-
 }
 
 async function register() {
@@ -150,11 +146,20 @@ async function register() {
   let regex = /^\w([\_\.0-9a-zA-Z]+)@([\_\.0-9a-zA-Z]+)\.([a-zA-z]){2,7}$/;
   if (reg.test(password)) {
     if (regex.test(email)) {
-      const { data } = await axios.post('/api/register', { user, email, password, cpassword });
+      const { data } = await axios.post("/api/register", {
+        user,
+        email,
+        password,
+        cpassword,
+      });
 
       localStorage.setItem("user", JSON.stringify(data));
       if (data === "password does not match") {
-        swal("Oops!", "Your Password And Confirm Password Does Not Match!", "error");
+        swal(
+          "Oops!",
+          "Your Password And Confirm Password Does Not Match!",
+          "error"
+        );
       } else if (data === "Account Already Exists") {
         swal("Oops!", "Account Already Exists!", "error");
       } else {
@@ -164,8 +169,11 @@ async function register() {
       swal("Oops!", "Email Is Not Valid", "error");
     }
   } else {
-    swal("Oops!", "Password Should Be Minimum 8 characters And Maximum 15 characters", "error");
-
+    swal(
+      "Oops!",
+      "Password Should Be Minimum 8 characters And Maximum 15 characters",
+      "error"
+    );
   }
 }
 
@@ -174,57 +182,69 @@ function update_profile(id) {
   let email = document.getElementById("email_profile").value;
   let regex = /^\w([\_\.0-9a-zA-Z]+)@([\_\.0-9a-zA-Z]+)\.([a-zA-z]){2,7}$/;
   if (regex.test(email)) {
-    axios.put(`api/login/${id}`, { user, email }).then(function (response) {
-      if(response.data==="invalid"){
-        swal("Oops!", "This Email is Already Exists You Can't Update'!", "error");
-      }else{
-        localStorage.setItem('name', response.data.user)
-      
-      window.location.href = "/";
-      }
-      
-    }).catch(function (error) {
-      console.log(error);
-    })
+    axios
+      .put(`api/login/${id}`, { user, email })
+      .then(function (response) {
+        if (response.data === "invalid") {
+          swal(
+            "Oops!",
+            "This Email is Already Exists You Can't Update'!",
+            "error"
+          );
+        } else {
+          localStorage.setItem("name", response.data.user);
+
+          window.location.href = "/";
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   } else {
     swal("Oops!", "Email Is Not Valid", "error");
   }
-
 }
 
 function postemail() {
-  const email = document.getElementById('emailpost').value;
+  const email = document.getElementById("emailpost").value;
 
-  axios.post(`/api/forget`, { email }).then((response) => {
-    if (response.data === "") {
-      swal("Oops!", "This Email Address Are Not Register", "error");
-    } else {
-      localStorage.setItem('userloginId', response.data._id.toString());
-      window.location.href = "/password";
-    }
-
-  }).catch((error) => {
-    console.log(error);
-  })
+  axios
+    .post(`/api/forget`, { email })
+    .then((response) => {
+      if (response.data === "") {
+        swal("Oops!", "This Email Address Are Not Register", "error");
+      } else {
+        localStorage.setItem("userloginId", response.data._id.toString());
+        window.location.href = "/password";
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 function updatePassword() {
   let password = document.getElementById("forgetpassword").value;
   let cpassword = document.getElementById("forgetcpassword").value;
-  let id = localStorage.getItem('userloginId');
+  let id = localStorage.getItem("userloginId");
   let reg = /([!@#$%^&*0-9a-zA-Z]+){8,15}/;
   if (reg.test(password)) {
-    axios.put(`/api/forget/${id}`, { password, cpassword }).then((data) => {
-      if (data.data === 'notMatch') {
-        swal("Oops!", "Your Password And Confirm Password Does Not Match!", "error");
-      } else {
-        window.location.href = '/login'
-      }
-
-    }).catch((error) => {
-      console.log(error);
-    })
+    axios
+      .put(`/api/forget/${id}`, { password, cpassword })
+      .then((data) => {
+        if (data.data === "notMatch") {
+          swal(
+            "Oops!",
+            "Your Password And Confirm Password Does Not Match!",
+            "error"
+          );
+        } else {
+          window.location.href = "/login";
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   } else {
     swal("Oops!", "Password Should Be  8 characters", "error");
   }
-
 }
--- services/controller/controll.js
+++ services/controller/controll.js
@@ -1,227 +1,233 @@
-const Blog = require('../model/model');
-const register = require('../model/register');
-const bcrypt = require('bcrypt');
-const localstorage = require('node-localstorage');
-
-
+const Blog = require("../model/model");
+const register = require("../model/register");
+const bcrypt = require("bcrypt");
+const localstorage = require("node-localstorage");
 
 exports.create = (req, res) => {
-    if (!req.body) {
-        res.status(404).send({ message: 'connect and not be find' });
-        return;
-    }
+  if (!req.body) {
+    res.status(404).send({ message: "connect and not be find" });
+    return;
+  }
 
+  const users = new Blog({
+    img: req.file.filename,
+    desc: req.body.desc,
+    title: req.body.title,
+    id: req.cookies.id,
+  });
 
-    const users = new Blog({
-        img: req.file.filename,
-        desc: req.body.desc,
-        title: req.body.title,
-        id: req.cookies.id
-
+  users
+    .save(users)
+    .then((data) => {
+      res.send(data);
+      // res.redirect('/')
     })
-
-    users.save(users).then((data) => {
-        res.send(data)
-        // res.redirect('/')
-    }).catch(err => {
-        console.log(err);
-    })
-}
+    .catch((err) => {
+      console.log(err);
+    });
+};
 exports.read = (req, res) => {
-    if (req.query.id) {
-        let id = req.query.id;
-        Blog.findById(id).then((data) => {
-            res.send(data);
-        }).catch(err => {
-            console.log(err);
-        })
-    }
-
-    else {
-        Blog.find().then((user) => {
-            res.send(user)
-        }).catch(err => {
-            console.log(err);
-        })
-    }
-
-}
+  if (req.query.id) {
+    let id = req.query.id;
+    Blog.findById(id)
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    Blog.find()
+      .then((user) => {
+        res.send(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+};
 
 exports.delete = (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndRemove(id).then((data) => {
-        if (!data) {
-            res.status(404).send({ message: `cannot be delete this ${id}` })
-        } else {
-            res.send({ message: "delete successfully" })
-        }
-
-    }).catch(function (err) {
-        res.send({ message: err.message })
+  const id = req.params.id;
+  Blog.findByIdAndRemove(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({ message: `cannot be delete this ${id}` });
+      } else {
+        res.send({ message: "delete successfully" });
+      }
     })
-
-}
+    .catch(function (err) {
+      res.send({ message: err.message });
+    });
+};
 exports.update = (req, res) => {
-    const { desc, title } = req.body;
-    let img = req.file.filename;
-    const updatedPost = { img, desc, title };
-    if (!req.body) {
-        res.status(404).send({ masssage: `cannot be empty` })
-        return;
-    }
-    const id = req.params.id;
-    console.log(updatedPost);
-    Blog.findByIdAndUpdate(id, updatedPost, { new: true })
-        .then(data => {
-            if (!data) {
-                res.status(404).send({ message: `Cannot Update user with ${id}. Maybe user not found!` })
-            } else {
-                res.send(data)
-            }
-        })
-        .catch(err => {
-            res.status(500).send({ message: "Error Update user information" })
-        })
+  const { desc, title } = req.body;
+  let img = req.file;
+  let updatedPost;
+ 
+  if (img === undefined) {
+    updatedPost = { desc, title };
+  } else {
+    
+    let img = req.file.filename
+    updatedPost = { img, desc, title };
+  }
+  console.log(updatedPost);
+  
+  const id = req.params.id;
 
-}
+  Blog.findByIdAndUpdate(id, updatedPost, { new: true })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot Update user with ${id}. Maybe user not found!`,
+        });
+      } else {
+        res.send(data);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ message: "Error Update user information" });
+    });
+};
 
 //here we are write post request to register user
 exports.post = (req, res) => {
-    if (!req.body) {
-        res.status(404).send({ message: `field con not be empty` })
-        return;
-    }
-    const password = req.body.password;
-    const cpassword = req.body.cpassword;
-    const email = req.body.email;
-    register.findOne({ email: email }).then((data) => {
-        if (!data) {
-            if (password === cpassword) {
-                const reg = new register({
-                    user: req.body.user,
-                    email: req.body.email,
-                    password: req.body.password,
-                    cpassword: req.body.cpassword,
+  if (!req.body) {
+    res.status(404).send({ message: `field con not be empty` });
+    return;
+  }
+  const password = req.body.password;
+  const cpassword = req.body.cpassword;
+  const email = req.body.email;
+  register
+    .findOne({ email: email })
+    .then((data) => {
+      if (!data) {
+        if (password === cpassword) {
+          const reg = new register({
+            user: req.body.user,
+            email: req.body.email,
+            password: req.body.password,
+            cpassword: req.body.cpassword,
+          });
 
-                })
-
-                reg.save(reg).then((data) => {
-                    res.send(data);
-                }).catch(err => {
-                    console.log(err);
-                })
-            } else {
-                res.send("password does not match")
-            }
+          reg
+            .save(reg)
+            .then((data) => {
+              res.send(data);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
         } else {
-            res.send("Account Already Exists")
+          res.send("password does not match");
         }
-    }).catch((err) => {
-        console.log(err);
+      } else {
+        res.send("Account Already Exists");
+      }
     })
-
-}
+    .catch((err) => {
+      console.log(err);
+    });
+};
 exports.readuser = (req, res) => {
-    
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    register.findOne({ email }).then((data) => {
+  register
+    .findOne({ email })
+    .then((data) => {
+      const ismatch = bcrypt.compareSync(password, data.password);
+      const token = data.registerToken();
 
+      res.cookie("jwt", token);
+      const id = data["id"].toString();
+      res.cookie("id", id);
 
-        const ismatch = bcrypt.compareSync(password, data.password);
-        const token = data.registerToken();
-
-        res.cookie('jwt', token)
-        const id = data['id'].toString();
-        res.cookie('id', id);
-
-        if (ismatch) {
-
-            res.status(200).send(data);
-        } else {
-            res.send("invalid response")
-        }
-
-    }).catch((err) => {
-        res.send("err")
+      if (ismatch) {
+        res.status(200).send(data);
+      } else {
+        res.send("invalid response");
+      }
     })
-
-
-}
+    .catch((err) => {
+      res.send("err");
+    });
+};
 
 exports.profile = (req, res) => {
-    
-    register.find().then((data) => {
-        res.send(data)
-    }).catch((err) => {
-        console.log(err)
+  register
+    .find()
+    .then((data) => {
+      res.send(data);
     })
-}
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 exports.profileUpdate = (req, res) => {
+  const user = req.body.user;
+  const email = req.body.email;
+  const id = req.params.id;
 
-    const user = req.body.user;
-    const email = req.body.email;
-    const id = req.params.id;
-
-  
-    register.findOne({email}).then((data) => {
-        
-        if (!data||(data._id.toString()==req.cookies.id)){
-            register.findByIdAndUpdate(id, { user, email }, { new: true }).then(data => {
-                if (!data) {
-                    res.status(404).send({ message: `Cannot Update user with ${id}. Maybe user not found!` })
-                } else {
-                    res.send(data)
-                }
-            })
-                .catch(err => {
-                    res.status(500).send({ message: "Error Update user information" })
-                })
-        }else{
-            res.send("invalid")
-        }
-    })
-   
-}
+  register.findOne({ email }).then((data) => {
+    if (!data || data._id.toString() == req.cookies.id) {
+      register
+        .findByIdAndUpdate(id, { user, email }, { new: true })
+        .then((data) => {
+          if (!data) {
+            res.status(404).send({
+              message: `Cannot Update user with ${id}. Maybe user not found!`,
+            });
+          } else {
+            res.send(data);
+          }
+        })
+        .catch((err) => {
+          res.status(500).send({ message: "Error Update user information" });
+        });
+    } else {
+      res.send("invalid");
+    }
+  });
+};
 
 exports.getRegistrUser = (req, res) => {
-    const email = req.body.email
-    register.findOne({ email }).then((data) => {
-
-        res.send(data)
-
-    }).catch((err) => {
-        res.send('err')
+  const email = req.body.email;
+  register
+    .findOne({ email })
+    .then((data) => {
+      res.send(data);
     })
-}
+    .catch((err) => {
+      res.send("err");
+    });
+};
 
 exports.forget = (req, res) => {
-
-    const id = req.params.id;
-    const userpassword = req.body.password;
-    const usercpassword = req.body.cpassword;
-    if (userpassword === usercpassword) {
-        const password = bcrypt.hashSync(userpassword, 10);
-        const cpassword = bcrypt.hashSync(usercpassword, 10);
-        register.findByIdAndUpdate(id, { password, cpassword }, { new: true }).then(data => {
-            if (!data) {
-                res.status(404).send({ message: `Cannot Update user with ${id}. Maybe user not found!` })
-            } else {
-                res.send(data)
-            }
-        })
-            .catch(err => {
-                res.status(500).send({ message: "Error Update user information" })
-            })
-    } else {
-        res.send("notMatch")
-    }
-}
-
-
-
-
-
-
-
+  const id = req.params.id;
+  const userpassword = req.body.password;
+  const usercpassword = req.body.cpassword;
+  if (userpassword === usercpassword) {
+    const password = bcrypt.hashSync(userpassword, 10);
+    const cpassword = bcrypt.hashSync(usercpassword, 10);
+    register
+      .findByIdAndUpdate(id, { password, cpassword }, { new: true })
+      .then((data) => {
+        if (!data) {
+          res.status(404).send({
+            message: `Cannot Update user with ${id}. Maybe user not found!`,
+          });
+        } else {
+          res.send(data);
+        }
+      })
+      .catch((err) => {
+        res.status(500).send({ message: "Error Update user information" });
+      });
+  } else {
+    res.send("notMatch");
+  }
+};
